050662784ba08dda2600b08138c02a5367d3b714
AhmedYousry55 <ahmedyousry200175@gmail.com>
2023-06-23 13:53:19 +0300

    twelvth commit

--- routes/coureseRoutes.js
+++ routes/coureseRoutes.js
@@ -6,11 +6,11 @@ const authController = require('./../controllers/authController');
 
 router
   .route('/')
-  .get(authController.restrictTo('student','staff','admin'),courseController.getAllCourses)
+  .get(authController.protect,authController.restrictTo('student','staff','admin'),courseController.getAllCourses)
   .post(authController.protect,authController.restrictTo('staff','admin'),courseController.createCourse);
 router
   .route('/:id')
-  .get(authController.restrictTo('student','staff','admin'),courseController.getOneCourse)
+  .get(authController.protect,authController.restrictTo('student','staff','admin'),courseController.getOneCourse)
   .patch(authController.protect,authController.restrictTo('staff','admin'),courseController.updateCourse)
   .delete(authController.protect,authController.restrictTo('staff','admin'),courseController.deleteCourse);
   router
